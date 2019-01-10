fa60cee96387c8d5b87894659990a65382f02be3
Grzegorz Kolek <gkolek@gazeta.pl>
2019-01-10 23:53:31 +0100

    checkbox-checking works (but paralelly, not serially to email-checking)

--- script/main.js
+++ script/main.js
@@ -1,20 +1,32 @@
 var emailForm = document.getElementById('emailForm')
-var getEmail = document.getElementById('getEmail');
+
+
+function myCheck() {
+    var checkBox = document.getElementById("myCheck");
+    var text = document.getElementById("remind");
+    if (checkBox.checked == true) {
+        text.style.display = "none";
+        return console.log('checkbox succeed')
+    } else {
+        text.style.display = "block";
+        return console.log('checkbox failed')
+    }
+}
+emailForm.addEventListener('submit', myCheck)
 
 function handleForm(event) {
     event.preventDefault()
 
+    var getEmail = document.getElementById('getEmail');
     var emailValue = getEmail.value;
     var testAt = emailValue.indexOf('@');
     var testDot = emailValue.indexOf('.');
 
     if (testAt > 0 && testDot > 0) {
         location.replace("./game-board.html")
-        return console.log('succeed')
+        return console.log('redirect succeed')
     }
-    return console.log('failed')
+    return console.log('redirect failed')
 }
-
 emailForm.addEventListener('submit', handleForm)
-
 
