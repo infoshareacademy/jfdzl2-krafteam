fd498083a5e75fec8434dbd6b7cee4a48067f10e
Grzegorz Kolek <gkolek@gazeta.pl>
2019-01-10 01:27:51 +0100

    @ checking works on click and enter

--- script/main.js
+++ script/main.js
@@ -1,10 +1,20 @@
-function checkEmailAddress() {
-    var getEmail = document.getElementById('getEmail');
+var emailForm = document.getElementById('emailForm')
+var getEmail = document.getElementById('getEmail');
+
+function handleForm(event) {
+    event.preventDefault()
+
     var emailValue = getEmail.value;
     var testAt = emailValue.indexOf('@');
-    var testDot = emailValue.indexOf('.')
+    var testDot = emailValue.indexOf('.');
 
     if (testAt > 0 && testDot > 0) {
         location.replace("./game-board.html")
+        return console.log('succeed')
     }
+    return console.log('failed')
 }
+
+emailForm.addEventListener('submit', handleForm)
+
+
