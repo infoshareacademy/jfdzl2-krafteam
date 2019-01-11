dacc4840098eafe10dc9a5dd0cb38f13f5ba720b
Grzegorz Kolek <gkolek@gazeta.pl>
2019-01-11 12:09:11 +0100

    done (checkbox-checking + email-checking works serially)

--- script/main.js
+++ script/main.js
@@ -2,31 +2,31 @@ var emailForm = document.getElementById('emailForm')
 
 
 function myCheck() {
-    var checkBox = document.getElementById("myCheck");
-    var text = document.getElementById("remind");
-    if (checkBox.checked == true) {
-        text.style.display = "none";
-        return console.log('checkbox succeed')
-    } else {
-        text.style.display = "block";
-        return console.log('checkbox failed')
-    }
 }
 emailForm.addEventListener('submit', myCheck)
 
 function handleForm(event) {
     event.preventDefault()
 
-    var getEmail = document.getElementById('getEmail');
-    var emailValue = getEmail.value;
-    var testAt = emailValue.indexOf('@');
-    var testDot = emailValue.indexOf('.');
+    var checkBox = document.getElementById("myCheck");
+    var myRemind = document.getElementById("remind");
+    if (checkBox.checked == true) {
+        myRemind.style.display = "none";
+        console.log('checkbox succeed')
+        var getEmail = document.getElementById('getEmail');
+        var emailValue = getEmail.value;
+        var testAt = emailValue.indexOf('@');
+        var testDot = emailValue.indexOf('.');
 
-    if (testAt > 0 && testDot > 0) {
-        location.replace("./game-board.html")
-        return console.log('redirect succeed')
+        if (testAt > 0 && testDot > 0) {
+            location.replace("./game-board.html")
+            console.log('redirect succeed')
+        }
+        console.log('redirect failed')
+    } else {
+        myRemind.style.display = "block";
+        console.log('checkbox failed')
     }
-    return console.log('redirect failed')
 }
 emailForm.addEventListener('submit', handleForm)
 
