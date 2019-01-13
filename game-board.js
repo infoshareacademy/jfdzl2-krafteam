11eafa6cd4834985cd6b74174bd365a9d4a44f90
Michalina Zeligowska <michalina.zeligowska@gmail.com>
2019-01-13 15:21:18 +0100

    eventlisteners added

---
+++ game-board.js
@@ -0,0 +1,57 @@
+class Player {
+    constructor() {
+        this.playerElement = document.querySelector('.gamer');
+        this.x = 0;
+        this.y = 0;
+        this.registerEvents();
+    }
+    moveRight() {
+        if (this.x < 20) {
+            this.x++;
+            this.playerElement.style.left = this.x * 5 + '%';
+            console.log('move right')
+        }
+    }
+    moveLeft() {
+        if (this.x > 0) {
+            this.x--;
+            this.playerElement.style.left = this.x * 5 + '%';
+            console.log('move left');
+        }
+    }
+    moveUp() {
+        if (this.y > 0) {
+            this.y--;
+            this.playerElement.style.top = this.y * 5 + '%';
+            console.log('move up');
+        }
+    }
+    moveDown() {
+        if (this.y < 20) {
+            this.y++;
+            this.playerElement.style.top = this.y * 5 + '%';
+            console.log('move down');
+        }
+    }
+    registerEvents() {
+        window.addEventListener('keydown', e => {
+            switch (e.key) {
+                case 'ArrowUp':
+                    this.moveUp();
+                    break;
+                case 'ArrowDown':
+                    this.moveDown();
+                    break;
+                case 'ArrowRight':
+                    this.moveRight();
+                    break;
+                case 'ArrowLeft':
+                    this.moveLeft();
+                    break;
+                default:
+                    return;
+            }
+        });
+    }
+}
+const newPlayer = new Player();
